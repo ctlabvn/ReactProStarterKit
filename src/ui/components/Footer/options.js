eade697928afd9348236a0111d046f5d81f4afe2
Trần Phú <svtxphu@gmail.com>
2017-10-13 16:28:04 +0700

    update langeuage on home screen

--- src/ui/components/Footer/options.js
+++ src/ui/components/Footer/options.js
@@ -3,7 +3,7 @@ export default {
     {title: 'Partner', link: '#'},
     {title: 'Help Center', link: '#'},
     {title: 'Support', link: '#'},
-    {title: ' Watch our video', link: '#'},
+    {title: 'Watch our video', link: '#'},
     {title: 'Terms & Conditions', link: '#'},
     {title: 'Privacy Policy', link: '#'},
     {title: 'About us', link: '#'},
