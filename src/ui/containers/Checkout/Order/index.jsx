19f78c170190ef0ee1b5ec1a06d6a8034ab98efa
Pham Thanh Tu <tubackkhoa@gmail.com>
2017-10-18 14:06:00 +0700

    format number

--- src/ui/containers/Checkout/Order/index.jsx
+++ src/ui/containers/Checkout/Order/index.jsx
@@ -1,27 +1,53 @@
-import React, { Component } from 'react';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import React, { Component } from "react";
+import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
+import { translate } from "react-i18next";
+import { connect } from "react-redux";
+import * as orderSelectors from "~/store/selectors/order";
 
 import options from "./options";
 
+@translate("translations")
+@connect(state => ({
+  orderItems: orderSelectors.getItems(state)
+}))
 export default class extends Component {
   render() {
+    const { orderItems, t } = this.props;
+    const orderItemsWithTotalPrice = orderItems.map(item => ({
+      ...item,
+      totalPrice: item.quantity * item.price
+    }));
+    const totalPrice = orderItemsWithTotalPrice.reduce(
+      (a, item) => a + item.totalPrice,
+      0
+    );
     return (
       <div>
         <h4>Your order</h4>
-        {options.products.map((item, index)=>
-          <div className="d-flex justify-content-start" key={index}>
-            <div className="p-2">1x</div>
-            <div className="p-2">{item}</div>
-            <div className="ml-auto p-2">$4</div>
+        {orderItemsWithTotalPrice.map(item => (
+          <div className="d-flex justify-content-start" key={item.item_uuid}>
+            <div className="p-2">{item.quantity}x</div>
+            <div className="p-2">{item.name}</div>
+            <div className="ml-auto p-2">
+              {t("format.currency", {
+                price: item.totalPrice,
+                symbol: item.currency_symbol
+              })}
+            </div>
           </div>
+        ))}
+        {orderItemsWithTotalPrice[0] && (
+          <dl className="d-flex justify-content-start">
+            <dt className="p-2 text-uppercase">Total</dt>
+            <dd className="ml-auto p-2 font-weight-bold">
+              {t("format.currency", {
+                price: totalPrice,
+                symbol: orderItemsWithTotalPrice[0].currency_symbol
+              })}
+            </dd>
+          </dl>
         )}
-        <dl className="d-flex justify-content-start">
-          <dt className="p-2 text-uppercase">Total</dt>
-          <dd className="ml-auto p-2 font-weight-bold">$14</dd>
-        </dl>
       </div>
-    )
+    );
   }
 }
-
-
