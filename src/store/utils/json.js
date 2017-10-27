e7435d2adf584673722e69f64e4befba700cb5f3
Trần Phú <svtxphu@gmail.com>
2017-10-27 11:58:23 +0700

    image display too small to view, then i comment it fix json parse, we will have maximum 2 turn for parsing

--- src/store/utils/json.js
+++ src/store/utils/json.js
@@ -4,19 +4,9 @@ export const parseJsonToObject = (str, defaultValueIfFalse = {}) => {
 	}
 
 	// we will have maximum 2 parse turns
-	let result;
 	try{
-		result = JSON.parse(str);
-	} catch(e){
-		return defaultValueIfFalse;
-	}
-
-	if(typeof result == 'object') {
-		return result;
-	}
-
-	try{
-		return JSON.parse(result);
+		const result = JSON.parse(str);
+		return typeof result === 'object' ? result : JSON.parse(result);
 	} catch(e){
 		return defaultValueIfFalse;
 	}
