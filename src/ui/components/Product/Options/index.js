9efd61c5937abc1507fb4bfff4d00772cddfdfd3
Trần Phú <svtxphu@gmail.com>
2017-11-01 10:54:12 +0700

    update view for order options

--- src/ui/components/Product/Options/index.js
+++ src/ui/components/Product/Options/index.js
@@ -9,6 +9,14 @@ import * as orderActions from "~/store/actions/order";
 @translate("translations")
 @connect(null, orderActions)
 export default class extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			form: {},
+			disableAddToCart: false
+		}
+	}
+
 	static propTypes = {
 		options: PropTypes.array,
 		currency: PropTypes.object,
@@ -34,61 +42,136 @@ export default class extends Component {
 		});
 	}
 
-	handleSubmit = (e) => {
-		e.preventDefault();
-		const { options } = this.props;
+	handleSubmit = () => {
+		// clean error
+		for(let spanTag of document.querySelectorAll('.text-error-msg')) {
+			spanTag.innerText = '';
+		}
 		// validate mandatory
-		for(let parent in options) {
-			if(!!parent.optionSet && parent.mandatory) {
-				let checkFlg = true;
-				if(parent.multiple_choice) {
-					for(let child in parent.optionSet) {
+		let validateResult = this.validateMandatory();
 
+		if(validateResult) {
+			// save into order
+			// clear checked
+		} else {
+			this.setState({
+				disableAddToCart: true
+			})
+		}
+	}
+
+	validateMandatory(showError = true) {
+		const { options } = this.props;
+		const { form } = this.state;
+		let validateResult = true;
+		console.log(showError, form);
+		for(let parent of options) {
+			if(!!parent.optionSet && parent.mandatory) {
+				// check state and show alert message
+				if(this.checkObjectFalse(form[parent.opt_set_uuid])) {
+					validateResult = false;
+					if(showError) {
+						document.querySelector('#error-' + parent.opt_set_uuid).innerText = 'Error, this is mandatory!';
 					}
-				} else {
-					console.log(document.querySelector('input[name]'))
 				}
 			}
 		}
+		return validateResult;
+	}
+
+	checkObjectFalse(obj) {
+		for(let objValue of Object.keys(obj)) {
+			if(obj[objValue]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	componentDidMount() {
+		const { options } = this.props;
+		let formTree = {};
+
+		for(let parent of options) {
+			if(!!parent.optionSet) {
+				formTree[parent.opt_set_uuid] = {};
+				for(let child of parent.optionSet) {
+					formTree[parent.opt_set_uuid][child.option_uuid] = false;
+				}
+			}
+		}
+		this.setState({form: formTree});
+	}
+
+	handleChange = (parentUuid, child, multiChoice) => {
+		const { disableAddToCart, form } = this.state;
+		if(disableAddToCart) {
+			this.setState({
+				disableAddToCart: this.validateMandatory(false)
+			})
+		}
+
+		if(!multiChoice) {
+			for(let childUuid of Object.keys(form[parentUuid])) {
+				form[parentUuid][childUuid] = false;
+			}
+		}
+		form[parentUuid][child.option_uuid] = form[parentUuid][child.option_uuid] ? false : child;
+		this.setState({
+			form: form
+		});
+	}
+
+	renderOption(symbol, parent, t) {
+		const inputType = parent.multiple_choice ? "checkbox" : "radio";
+		return (
+			<div className="col-10 row">
+				{parent.optionSet.map((child, index) => {
+					const inputName = parent.multiple_choice ? `data[${parent.id}][${child.id}]` : `data[${parent.id}]`;
+					return (
+						<div className="col-3" key={index}>
+							<p>
+								<input key={child.option_uuid} id={child.option_uuid} name={inputName}
+								       onChange={() => this.handleChange(parent.opt_set_uuid, child, parent.multiple_choice)} type={inputType} className="mr-1"/>
+								<label htmlFor={child.option_uuid}>{child.name}</label>
+								{child.price > 0 && (
+									<small className="badge badge-danger">
+										{t("format.currency", {
+											price: child.price,
+											symbol: symbol
+										})}
+									</small>
+								)}
+							</p>
+						</div>
+					)
+				})}
+			</div>
+		);
 	}
 
 	render() {
 		const { t, canAddOrder, item, options, currency } = this.props;
+		const { disableAddToCart } = this.state;
+		const symbol = currency.symbol ? currency.symbol : '₫';
 		return (
-			<form id="add-options" onSubmit={this.handleSubmit}>
+			<div id="add-options">
 				{options.map(parent => (
 					<div className="row my-3 border-bottom">
-						<div className="col-3">
+						<div className="col-2">
 							<span className={classNames("", parent.mandatory ? "border-bottom border-danger" : "")}>{parent.name}</span>
 						</div>
-						<div className="col-9 row">
-							{!!parent.optionSet && parent.optionSet.map((child, index) => (
-								<div className="col-lg-4" key={index}>
-									<p>
-										<input key={child.option_uuid} id={child.option_uuid} name={parent.multiple_choice ? `${parent.id}[${child.id}]` : parent.id} type={parent.multiple_choice ? "checkbox" : "radio"} className="mr-1" />
-										<label htmlFor={child.option_uuid}>{child.name}</label>
-										{child.price > 0 && (
-											<small className="badge badge-danger">
-												{t("format.currency", {
-													price: child.price,
-													symbol: currency.symbol ? currency.symbol : '₫'
-												})}
-											</small>
-										)}
-									</p>
-								</div>
-							))}
-						</div>
+						{!!parent.optionSet && this.renderOption(symbol, parent, t)}
 						<div className="col-12">
-							<span id={`error-${parent.id}`}></span>
+							<span className="text-danger text-error-msg" id={`error-${parent.opt_set_uuid}`}></span>
 						</div>
 					</div>
 				))}
 				<div className="form-group text-right">
-					{!!canAddOrder && <button className="btn btn-danger btn-lg"
-					                          onClick={() => this.addOrderItem(item)}>{t('BUTTON.ADD_TO_CART')}</button>}
+					{!!canAddOrder && <button className="btn btn-danger btn-lg" disabled={disableAddToCart}
+					                          onClick={() => this.handleSubmit()}>{t('BUTTON.ADD_TO_CART')}</button>}
 				</div>
-			</form>
+			</div>
 		);
 	}
 }
