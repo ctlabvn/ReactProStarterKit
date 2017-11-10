737146acccbba940fda803c3cf2bf331076ecc62
Pham Thanh Tu <tubackkhoa@gmail.com>
2017-11-10 14:54:53 +0700

    currently set to 0 minutes of now

--- src/ui/containers/Checkout/index.jsx
+++ src/ui/containers/Checkout/index.jsx
@@ -54,7 +54,7 @@ export default class extends Component {
       requestor, clearItems, setToast} = this.props;    
     // const addressItem = address.find(item=>item.cus_address_uuid === orderInfo.cus_address_uuid);
     const now = moment();
-    const minutesOfNow = (now.hour() * 60 + now.minute());
+    const minutesOfNow = 0;//(now.hour() * 60 + now.minute());
     let minutesOfPreparation = orderInfo.request_time + orderInfo.preparation_time;
     if(orderInfo.order_type === ORDER_TYPE.DELIVERY){
       minutesOfPreparation += +orderInfo.travel_time;
