64713a5242c8b354bdb5cfcc91684f7ba65e6da2
Pham Thanh Tu <tubackkhoa@gmail.com>
2017-11-22 18:27:34 +0700

    default is dev

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -13,8 +13,10 @@ export const initialState = {
     language: "vi"
   },
   address: [],
-  config: {},
-  filters: {},
+  config: {
+    mode: "dev"
+  },
+  filters: {}
 };
 
 // Takes care of changing the application state
@@ -23,17 +25,20 @@ export const auth = (state = initialState, { type, payload }) => {
   switch (type) {
     case "app/setLanguage":
       return { ...state, customer: { ...state.customer, language: payload } };
-    case 'app/updateConfig':
-		  return {...state, config: {...state.config, [payload.key]: payload.value}};
-    case 'app/updateFilters':
-		  return {...state, filters: {...state.config, ...payload}};
+    case "app/updateConfig":
+      return {
+        ...state,
+        config: { ...state.config, [payload.key]: payload.value }
+      };
+    case "app/updateFilters":
+      return { ...state, filters: { ...state.config, ...payload } };
     case "app/setAuthState":
       return { ...state, loggedIn: payload };
     case "app/saveLoggedUser":
       return { ...state, ...payload }; // {user,token}
     case "app/removeLoggedUser":
       // remove only customer and remain language for saving spaces?
-      return { ...state, customer: {language: state.customer.language} };
+      return { ...state, customer: { language: state.customer.language } };
     case "app/saveRefreshToken":
       // payload is access token
       return { ...state, token: { ...state.token, ...payload } };
@@ -41,7 +46,7 @@ export const auth = (state = initialState, { type, payload }) => {
     // update customer
     case "customer/updateCustomer":
       // remove address from customer data
-      const {address, ...data} = payload;      
+      const { address, ...data } = payload;
       return { ...state, customer: { ...state.customer, ...data } };
     case "customer/addAddress":
       return { ...state, address: [...state.address, payload] };
