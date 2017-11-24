09933762f59f88b93567f1ca6fdf9fe120ee10d7
Pham Thanh Tu <tubackkhoa@gmail.com>
2017-11-24 23:19:52 +0700

    change view server

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const express = require("express"),
 // console.log(publicPath);
 // set port and engine
 app.set("port", process.env.PORT || 80);
-app.set("views", "./views");
+app.set("views", path.join(__dirname, "./views"));
 app.set("view engine", "pug");
 
 app.get("*", function(req, res) {
