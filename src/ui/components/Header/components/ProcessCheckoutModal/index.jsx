7a2e775434c3cfbec2754b3a8775ffb97d75ddeb
trung <trungdt@dft.vn>
2017-12-20 23:51:07 +0700

    fix bug

--- src/ui/components/Header/components/ProcessCheckoutModal/index.jsx
+++ src/ui/components/Header/components/ProcessCheckoutModal/index.jsx
@@ -170,87 +170,95 @@ export default class extends Component {
         className={this.props.className}
       >
         <ModalBody className="p-0">
-          <div className="checkout-right">
-            <div className="h-100">
-              <div className="">
-                <div className="color-cg-040 font-fb-120 text-center text-uppercase">{ORDER_TYPE.getString(orderInfo.order_type)}</div>
-                <div className="color-red font-fb-250 text-center">{
-                  t("format.currency", {
-                    price: orderPrices.total,
-                    symbol: currency_symbol
-                  })
-                }</div>
-              </div>
-
-              <div className="bg-white pl-md-4 pb-md-1 pt-md-1">
-                {!!restaurant_name && <div className="color-cg-040 font-fb-140 text-uppercase mb-2 mt-2">{restaurant_name}</div>}
-                {!!restaurant_address && <div className="color-red font-fr-120">{restaurant_address}</div>}
-                {!!restaurant_phone && <div className="color-red font-fr-120">{restaurant_phone}</div>}
-              </div>
-
-              <div className="">
-                <GoogleMapKey
-                  onItemRef={this.initGmap}
-                  height={225}
-                  defaultCenter={position}
-                >
-                  {directions ? (
-                    <DirectionsRenderer directions={directions}/>
-                  ) : position.lat && position.lng ? (
-                    <Marker position={position}/>
-                  ) : (
-                    <span className="w-100 text-center text-danger vertical-center">
-                      Can not address the location of business
-                      </span>
-                  )}
-                </GoogleMapKey>
-              </div>
-
-              <div className="bg-white pr-md-4 pb-md-1 pt-md-1 text-right">
-                {!!customer_name && <div className="color-cg-040 font-fb-140 text-uppercase mb-2 mt-2">
-                  {customer_name}
-                </div>}
-                {!!orderInfo && !!orderInfo.order_address && <div className="color-red font-fr-120">
-                  {orderInfo.order_address}
-                </div>}
-                {!!customer_phone && <div className="color-red font-fr-120">{customer_phone}</div>}
-              </div>
-
-              <div className="p-md-4 checkout-list-item">
-                {!!items && orderItemsWithTotalPrice.map((item, index) => (
-                  <CheckoutItem
-                    key={index}
-                    item={item}
-                    data={items[item.item_uuid]}
-                    t={t}
-                    onIncreaseOrder={onIncreaseOrder}
-                    onDecreaseOrder={onDecreaseOrder}
-                    onChangeQuantity={onChangeQuantity}
-                  />
-                ))}
-              </div>
-
-              <div className="px-md-4 pb-md-5">
-                {orderInfo.order_type === ORDER_TYPE.DELIVERY &&
-                <div className="d-flex justify-content-between text-uppercase font-fr-140 color-cg-040">
-                  <div>delivery fee</div>
-                  <div className="font-fr-130 color-red">{
+          {!!currency_symbol ? (
+            <div className="checkout-right">
+              <div className="h-100">
+                <div className="">
+                  <div className="color-cg-040 font-fb-120 text-center text-uppercase">{ORDER_TYPE.getString(orderInfo.order_type)}</div>
+                  <div className="color-red font-fb-250 text-center">{
                     t("format.currency", {
-                      price: orderPrices.fee,
+                      price: orderPrices.total,
                       symbol: currency_symbol
                     })
                   }</div>
                 </div>
-                }
+
+                <div className="bg-white pl-md-4 pb-md-1 pt-md-1">
+                  {!!restaurant_name && <div className="color-cg-040 font-fb-140 text-uppercase mb-2 mt-2">{restaurant_name}</div>}
+                  {!!restaurant_address && <div className="color-red font-fr-120">{restaurant_address}</div>}
+                  {!!restaurant_phone && <div className="color-red font-fr-120">{restaurant_phone}</div>}
+                </div>
+
+                <div className="">
+                  <GoogleMapKey
+                    onItemRef={this.initGmap}
+                    height={225}
+                    defaultCenter={position}
+                  >
+                    {directions ? (
+                      <DirectionsRenderer directions={directions}/>
+                    ) : position.lat && position.lng ? (
+                      <Marker position={position}/>
+                    ) : (
+                      <span className="w-100 text-center text-danger vertical-center">
+                      Can not address the location of business
+                      </span>
+                    )}
+                  </GoogleMapKey>
+                </div>
+
+                <div className="bg-white pr-md-4 pb-md-1 pt-md-1 text-right">
+                  {!!customer_name && <div className="color-cg-040 font-fb-140 text-uppercase mb-2 mt-2">
+                    {customer_name}
+                  </div>}
+                  {!!orderInfo && !!orderInfo.order_address && <div className="color-red font-fr-120">
+                    {orderInfo.order_address}
+                  </div>}
+                  {!!customer_phone && <div className="color-red font-fr-120">{customer_phone}</div>}
+                </div>
+
+                <div className="p-md-4 checkout-list-item">
+                  {!!items && orderItemsWithTotalPrice.map((item, index) => (
+                    <CheckoutItem
+                      key={index}
+                      item={item}
+                      data={items[item.item_uuid]}
+                      t={t}
+                      onIncreaseOrder={onIncreaseOrder}
+                      onDecreaseOrder={onDecreaseOrder}
+                      onChangeQuantity={onChangeQuantity}
+                    />
+                  ))}
+                </div>
+
+                <div className="px-md-4 pb-md-5">
+                  {orderInfo.order_type === ORDER_TYPE.DELIVERY &&
+                  <div className="d-flex justify-content-between text-uppercase font-fr-140 color-cg-040">
+                    <div>delivery fee</div>
+                    <div className="font-fr-130 color-red">{
+                      t("format.currency", {
+                        price: orderPrices.fee,
+                        symbol: currency_symbol
+                      })
+                    }</div>
+                  </div>
+                  }
+                </div>
+                <Button
+                  className="process-checkout-btn font-fr-120 text-uppercase"
+                  onClick={this.checkoutBtnOnClick}
+                >
+                  {t("BUTTON.GO_TO_CART")}
+                </Button>
               </div>
-              <Button
-                className="process-checkout-btn font-fr-120 text-uppercase"
-                onClick={this.checkoutBtnOnClick}
-              >
-                {t("BUTTON.GO_TO_CART")}
-              </Button>
             </div>
-          </div>
+          ) : <div className="text-center p-2">
+            <img src="/images/no-data.png" height="100" alt="" />
+            <p className="color-gray text-uppercase">
+              {t("LABEL.CART_EMPTY")}
+            </p>
+          </div>}
+
         </ModalBody>
       </Modal>
     );
